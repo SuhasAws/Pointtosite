6f63f4f74e46499ba9867f986ec5decb90fc1acd
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-08-11 15:30:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,3 +44,27 @@ module "pip" {
     LBRule-backend-port                 = var.LBRule-backend-port 
     NSG-name              = var.NSG-name
 }
+
+module "PTS-sb" {
+  source = "./modules/PointToSiteVpn"
+  resource-group-name = module.resource-group.resource-group-name
+  location = module.resource-group.location
+  vnet-name = module.vnet.vnet-name
+  PTS-sb-name = var.PTS-sb-name 
+  PTS-sb-address_prefixes = var.PTS-sb-address_prefixes
+  PTS-LNGonpremise-name = var.PTS-LNGonpremise-name
+  PTS-LNGonpremise-gateway_address = var.PTS-LNGonpremise-gateway_address
+  PTS-LNGonpremise-address_space = var.PTS-LNGonpremise-address_space
+  PTS-pip-name = var.PTS-pip-name
+  PTS-pip-allocation_method = var.PTS-pip-allocation_method
+  PTS-VNG-name = var.PTS-VNG-name
+  PTS-VNG-type = var.PTS-VNG-typ
+  PTS-VNG-vpn_type = var.PTS-VNG-vpn_type 
+  PTS-VNG-active_active = var.PTS-VNG-active_active
+  PTS-VNG-enable_bgp = var.PTS-VNG-enable_bgp
+  PTS-VNG-sku = var.PTS-VNG-sku
+  PTS-VNG-ipc-private_ip_address_allocation = var.PTS-VNG-ipc-private_ip_address_allocation
+  PTS-VNGC-name = var.PTS-VNGC-name
+  PTS-VNGC-type = var.PTS-VNGC-type
+  PTS-VNGC-shared_key = var.PTS-VNGC-shared_key 
+}
